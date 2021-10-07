fa5a37eae4f92220e9e9a202887b0c293d2bbdf1
phienlee <phienlee3103@gmail.com>
2021-10-07 09:47:24 +0700

    update

--- module_2/src/_03_class_object_java/exercise/Fan.java
+++ module_2/src/_03_class_object_java/exercise/Fan.java
@@ -79,15 +79,15 @@ public class Fan {
     public static void main(String[] args) {
         Fan fan1 = new Fan();
         Fan fan2 = new Fan();
-        fan1.setOn(ON);
-        fan1.setSpeed(FAST);
+        fan1.setOn(Fan.ON);
+        fan1.setSpeed(Fan.FAST);
         fan1.setRadius(10);
         fan1.setColor("Yellow");
-        System.out.println("Fan 1 info"+ fan1);
-        fan2.setOn(OFF);
-        fan2.setSpeed(MEDIUM);
+        System.out.println("Fan 1 info"+ fan1.toString());
+        fan2.setOn(Fan.OFF);
+        fan2.setSpeed(Fan.MEDIUM);
         fan2.setRadius(5);
         fan2.setColor("Blue");
-        System.out.println("Fan 2 info"+ fan2);
+        System.out.println("Fan 2 info"+ fan2.toString());
     }
 }
