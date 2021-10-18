82d13d67bb5d7fd1f469171eed1b71d3c23d2cd1
phienlee <phienlee3103@gmail.com>
2021-10-18 23:35:21 +0700

    update exercise

---
+++ module_2/src/_09_dsa_stack_queue/practice/Node.java
@@ -0,0 +1,11 @@
+package _09_dsa_stack_queue.practice;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
